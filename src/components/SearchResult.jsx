74e8a2a9e937b189d5bb3c44cff3eae28321c16d
Shivam Chaurasiya <chshivam53@gmail.com>
2024-08-21 01:23:31 +0530

    final commit

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -18,13 +18,26 @@ const SearchResult = () => {
 
   const fetchSearchResults = () => {
     setLoading(true);
-    fetchDataFromApi(`search/q?=${searchQuery}`).then((res) => {
+    fetchDataFromApi(`search/?q=${searchQuery}`).then((res) => {
       setResult(res?.contents);
       setLoading(false);
     });
   };
 
-  return <div>SearchResult</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {result?.map((item) => {
+            if (item.type !== "video") return false;
+            let video = item.video;
+            return <SearchResultVideoCard key={video?.videoId} video={video} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResult;
